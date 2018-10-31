50b20448194b51ed4ca9f176327bca0abb458ca7
MitchellBerry <mitchellberry@protonmail.com>
2018-10-31 14:53:10 +0700

    added random_i32 and minmax

--- src/zx/random.rs
+++ src/zx/random.rs
@@ -1,2 +1,19 @@
 extern crate rand;
 
+use rand;
+
+
+pub fn random_i32()-> i32{
+    let mut rng = rand::thread_rng();
+    rng.gen::<i32>();
+}
+
+// minmax swaps x[0] and y[0] if y[0] < x[0]
+#[inline]
+pub fn min_max(x: &mut[i32], y: &mut[i32]){
+    if y[0] < x[0]{
+        let temp_x = x[0];
+        x[0] = y[0];
+        y[0] = temp_x;
+    }
+}
