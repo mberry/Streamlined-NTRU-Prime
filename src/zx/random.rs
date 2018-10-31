b2c51557a899b19b009eead9bd58bf23d4d582ba
MitchellBerry <mitchellberry@protonmail.com>
2018-10-31 15:13:15 +0700

    sort function added

--- src/zx/random.rs
+++ src/zx/random.rs
@@ -17,3 +17,33 @@ pub fn min_max(x: &mut[i32], y: &mut[i32]){
         y[0] = temp_x;
     }
 }
+
+//messy
+pub fn sort (x: &mut[i32], n isize){
+    if n < 2 {
+        break;
+    }
+    //floor(log2 n)
+    let top = 1;
+    for top < n-top {
+        top += top;
+    }
+    let mut p = top;
+    while p > 0 {
+        for i in 0..n-p{
+            if i & p == 0 {
+                min_max(x[i..], x[i+p..])
+            }
+        }
+        let mut q = top;    
+        while q > p {
+            for i in 0..n-q{
+                if i & p == 0 {
+                    min_max(x[(i+p)..], x[(i+q)..])
+                }
+            }
+            q >>= 1;
+        }
+        p >>= 1;
+    }
+}
