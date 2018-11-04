368013384ff06d4f9dc84ce4f2185f26e2a015f7
MitchellBerry <mitchellberry@protonmail.com>
2018-11-04 12:28:32 +0700

    fixed double borrow of minmax()

--- src/zx/encoding.rs
+++ src/zx/encoding.rs
@@ -1,9 +1,9 @@
 
 pub fn encode(f: [i8; 761])-> [u8; 191]{
-    let c = [0u8; 191];
+    let mut c = [0u8; 191];
     let mut j = 0;
     for i in 0..190{
-        let c0 = f[j] + 1;
+        let mut c0 = f[j] + 1;
         c0 += (f[j+1] + 1) << 2;
         c0 += (f[j+2] + 1) << 4;
         c0 += (f[j+3] + 1) << 6;
@@ -15,6 +15,19 @@ pub fn encode(f: [i8; 761])-> [u8; 191]{
 }
 
 fn decode(c: &[u8])-> [i8; 761]{
-    let f = [0i8; 761];
-
+    let mut f = [0i8; 761];
+    let mut j = 0;
+    for i in 0..190{
+        let mut c0 = c[i];
+        f[j] = ((c0 & 3) - 1) as i8;
+        c0 >>= 2;
+        f[j+1] = ((c0 & 3) - 1) as i8;
+        c0 >>= 2;
+        f[j+2] = ((c0 & 3) - 1) as i8;
+        c0 >>= 2;
+        f[j+3] = ((c0 & 3) - 1) as i8;
+        j += 4;
+    }
+    f[760] = ((c[190] & 3) -1 ) as i8;
+    f
 }
--- src/zx/random.rs
+++ src/zx/random.rs
@@ -7,12 +7,18 @@ pub fn random_i32()-> i32{
 }
 
 // minmax swaps x[0] and y[0] if y[0] < x[0]
-#[inline]
-pub fn min_max(x: &mut[i32], y: &mut[i32]){
-    if y[0] < x[0]{
-        let tmp_x = x[0];
-        x[0] = y[0];
-        y[0] = tmp_x;
+// #[inline]
+// pub fn min_max(x: &mut[i32], y: &mut[i32]){
+//     if y[0] < x[0]{
+//         let tmp_x = x[0];
+//         x[0] = y[0];
+//         y[0] = tmp_x;
+//     }
+// }
+
+pub fn min_max(arr: &mut[i32], x: usize, y: usize){
+    if arr[y] < arr[x]{
+        arr.swap(x, y);
     }
 }
 
@@ -21,7 +27,7 @@ pub fn sort (x: &mut[i32], n: usize){
         return
     }
     //floor(log2 n)
-    let top = 1;
+    let mut top = 1;
     while top < n-top {
         top += top;
     }
@@ -29,14 +35,16 @@ pub fn sort (x: &mut[i32], n: usize){
     while p > 0 {
         for i in 0..n-p{
             if i & p == 0 {
-                min_max(&mut x[i..], &mut x[i+p..])
+                //min_max(&mut x[i..], &mut x[i+p..])
+                min_max(x, i, i+p)
             }
         }
         let mut q = top;    
         while q > p {
             for i in 0..n-q{
                 if i & p == 0 {
-                    min_max(&mut x[(i+p)..], &mut x[(i+q)..])
+                    //min_max(&mut x[(i+p)..], &mut x[(i+q)..])
+                    min_max(x, i+p, i+q)
                 }
             }
             q >>= 1;
