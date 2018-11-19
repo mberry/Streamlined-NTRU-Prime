f44a5dc8ea682bebbb47a17ed7e1de1056ab446b
MitchellBerry <mitchellberry@protonmail.com>
2018-11-19 16:58:07 +0700

    tidy up tests

--- src/lib.rs
+++ src/lib.rs
@@ -129,32 +129,36 @@ mod tests {
     fn decap_kats(){
         let kats = parse_kat_file();
         for (i, kat) in kats.into_iter().enumerate(){
-            let katc = c_vec_to_array(hex::decode(&kat.c).unwrap());
-            let katsk = sk_vec_to_array(hex::decode(&kat.sk).unwrap());
-            let (k, _) = decapsulate(katc, katsk);
-            println!("KAT #: {}", i+1);
+            let ct = ct_to_arr(&kat.c);
+            let sk = sk_to_arr(&kat.sk);
+            let (k, _) = decapsulate(ct, sk);
+            println!("Decap KAT #: {}", i+1);
             println!("c: {}\n", kat.c);
             println!("sk: {}\n", kat.sk);
             println!("expected k: {}", kat.k);
             println!("decapped k: {}\n", hex::encode(k).to_uppercase());
-            assert_eq!(k , k_vec_to_array(hex::decode(kat.k).unwrap()));
+            assert_eq!(k , k_to_arr(&kat.k));
         }
-    
     }
 
     #[test]
     fn encap_kats(){
         let kats = parse_kat_file();
-        let pk = pk_vec_to_arr(hex::decode(&kats[0].pk).unwrap()); 
-        let sk = sk_vec_to_array(hex::decode(&kats[0].sk).unwrap()); 
-        let (c, k) = encapsulate(pk);
-        let (result, _) = decapsulate(c, sk);
-        assert_eq!(result, k)
+        for (i, kat) in kats.into_iter().enumerate(){
+            let pk = pk_to_arr(&kat.pk); 
+            let (ct, k) = encapsulate(pk);
+            let sk = sk_to_arr(&kat.sk); 
+            let (expected_k, _) = decapsulate(ct, sk);
+            println!("Encap KAT #: {}", i+1);
+            println!("encapped k: {}", hex::encode(k));
+            println!("expected k: {}\n", hex::encode(expected_k));
+            assert_eq!(expected_k, k)
+        }
     }
 
     #[test]
     fn keygentest() {
-        for _ in 0..10{
+        for _ in 0..5{
             let (pk, sk) = generate_key();
             let (c, k) = encapsulate(pk);
             let (result, _) = decapsulate(c, sk);
@@ -162,28 +166,27 @@ mod tests {
         }
     }
 
-    fn c_vec_to_array(v: Vec<u8>)-> [u8; 1047]{
-        let mut arr = [0u8; 1047];
-        arr.copy_from_slice(&v[..]);
+    fn ct_to_arr(s: &str)-> [u8; CT_SIZE]{
+        let mut arr = [0u8; CT_SIZE];
+        arr.copy_from_slice(&hex::decode(s).unwrap()[..]);
         arr
     }
 
-    fn sk_vec_to_array(v: Vec<u8>)-> [u8;1600]{
-        let mut arr = [0u8; 1600];
-        arr.copy_from_slice(&v[..]);
+    fn sk_to_arr(s: &str)-> [u8; SK_SIZE]{
+        let mut arr = [0u8; SK_SIZE];
+        arr.copy_from_slice(&hex::decode(s).unwrap()[..]);
         arr
     }
 
-    fn k_vec_to_array(v: Vec<u8>)-> [u8;32]{
-        let mut arr = [0u8; 32];
-        arr.copy_from_slice(&v[..]);
+    fn k_to_arr(s: &str)-> [u8; K_SIZE]{
+        let mut arr = [0u8; K_SIZE];
+        arr.copy_from_slice(&hex::decode(s).unwrap()[..]);
         arr
     }
 
-    fn pk_vec_to_arr(pk: Vec<u8>)->[u8; 1218]{
-        let mut arr = [0u8; 1218];
-        arr.copy_from_slice(&pk[..]);
+    fn pk_to_arr(s: &str)-> [u8; PK_SIZE]{
+        let mut arr = [0u8; PK_SIZE];
+        arr.copy_from_slice(&hex::decode(s).unwrap()[..]);
         arr
     }
-
 }
