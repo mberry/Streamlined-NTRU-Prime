aba210a2052ea6c002aa0ae65592e7c47b193c6f
MitchellBerry <mitchellberry@protonmail.com>
2018-11-20 00:23:53 +0700

    added benches

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,13 +1,21 @@
+#![feature(test)]
+
+#[macro_use]
+#[cfg(feature="tests")]
+extern crate serde_derive;
+
 #[cfg(test)]
 #[cfg(feature="tests")]
-mod tests {
+mod tests{
     extern crate serde_json;
     extern crate hex;
+    extern crate streamlined_ntru_prime as sntrup;
+    extern crate test;
 
+    use test::Bencher;
+    use self::sntrup::*;
     use std::fs::File;
-    use super::*;
-
-
+    
     #[derive(Deserialize)]
     struct KAT {
         c: String,
@@ -17,7 +25,8 @@ mod tests {
     }
 
     fn parse_kat_file()-> Vec<KAT>{
-        let f = File::open("src/tests/kat.json").expect("kat.json not found");
+        let path = "tests/kat.json";
+        let f = File::open(path).expect(&format!("kat.json not found: {}", path));
         serde_json::from_reader(f).expect("Error reading kat.json")
     }
 
@@ -60,6 +69,23 @@ mod tests {
             let (result, _) = decapsulate(c, sk);
             assert_eq!(result, k);
         }
+    }
+
+    #[bench]
+    fn key_gen_bench(b: &mut Bencher){
+        b.iter(|| generate_key());
+    }
+
+    #[bench]
+    fn encapsulate_bench(b: &mut Bencher){
+        let kat = &parse_kat_file()[0];
+        b.iter(|| encapsulate(pk_to_arr(&kat.pk)));
+    }
+
+    #[bench]
+    fn decapsulate_bench(b: &mut Bencher){
+        let kat = &parse_kat_file()[2];
+        b.iter(|| decapsulate(ct_to_arr(&kat.c), sk_to_arr(&kat.sk)));
     }
 
     fn ct_to_arr(s: &str)-> [u8; CT_SIZE]{
