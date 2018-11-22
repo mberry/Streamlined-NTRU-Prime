c6c41ebfb39dc3b576a3bc7d5b8c4cec33458404
MitchellBerry <mitchellberry@protonmail.com>
2018-11-22 09:45:03 +0700

    added doctests

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,11 @@ pub const P : usize = 761;
 pub const Q : usize = 4591;
 pub const W: usize = 286;
 
+///Generates a public and private keypair
+/// # Example
+/// ```
+/// let (public_key, private_key) = generate_key();
+/// ```
 pub fn generate_key()->([u8; PK_SIZE], [u8; SK_SIZE]){
     let mut rng = rand::thread_rng();
     let mut g = [0i8; P];
@@ -31,6 +36,12 @@ pub fn generate_key()->([u8; PK_SIZE], [u8; SK_SIZE]){
     derive_key(f, g, gr)
 }
 
+/// Encapsulates a public key.
+/// Returns a ciphertext and shared key
+/// # Example
+/// ```
+/// let (cipher_text, shared_secret) = encapsulate(public_key);
+/// ```
 pub fn encapsulate(pk : [u8; PK_SIZE])-> ([u8; CT_SIZE], [u8; K_SIZE]){
     let mut r = [0i8; P];
     let mut rng = rand::thread_rng();
@@ -38,7 +49,13 @@ pub fn encapsulate(pk : [u8; PK_SIZE])-> ([u8; CT_SIZE], [u8; K_SIZE]){
     create_cipher(r, pk)
 }
 
-pub fn decapsulate(cstr: [u8; CT_SIZE], sk: [u8; SK_SIZE])-> ([u8; K_SIZE], bool){
+/// Decapsulates ciphertext with a known private key.
+/// Returns a result containing the shared key.
+/// # Example
+/// ```
+/// let shared_secret = decapsulate(cipher_text, private_key).expect("Decapsulation error") 
+/// ```
+pub fn decapsulate(cstr: [u8; CT_SIZE], sk: [u8; SK_SIZE])-> Result<[u8; K_SIZE], bool>{
     let f = zx::encoding::decode(&sk[..191]);
     let c = rq::encoding::decode_rounded(&cstr[32..]);
     let mut t = [0i16; P];
@@ -51,19 +68,19 @@ pub fn decapsulate(cstr: [u8; CT_SIZE], sk: [u8; SK_SIZE])-> ([u8; K_SIZE], bool
     let mut r = [0i8; P];
     r3::mult(&mut r, t3, gr);
     let w = count_zeroes(r);
-    let mut ok = w == 286;
+    let mut check = w == 286;
     let h = rq::encoding::decode(&sk[(2 * 191)..]);
     let mut hr = [0i16; P];
     rq::mult(&mut hr, h, r);
     rq::round3(&mut hr);
     for i in 0..P{
-        ok &= (hr[i] - c[i]) == 0;
+        check &= (hr[i] - c[i]) == 0;
     }
     let s = Sha512::digest(&zx::encoding::encode(r));
-    ok &= s[..32] == cstr[..32];
+    check &= s[..32] == cstr[..32];
     let mut k = [0u8; 32];
     k.copy_from_slice(&s[32..]);
-    (k, ok)
+    if check { Ok(k) } else { Err(false) }
 }
 
 fn count_zeroes(r: [i8; P])-> i32{
